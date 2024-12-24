afaf8b1594d737184dcdec9c78e8d89ef8572d45
Sanjit Ghosh <sanjitkumarghosh0@gmail.com>
2024-12-25 02:24:25 +0600

    integrate update review

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -5,15 +5,34 @@ import {
   Typography,
   Avatar,
   Rating,
+  Textarea,
+} from "@material-tailwind/react";
+import {
+  Input,
+  Button,
+  Dialog,
+  IconButton,
+  DialogBody,
+  DialogHeader,
+  DialogFooter,
 } from "@material-tailwind/react";
 import axios from "axios";
 import { format } from "date-fns";
+import { useState } from "react";
 import { FiEdit3 } from "react-icons/fi";
 import { HiOutlineTrash } from "react-icons/hi2";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
+import useAuth from "../hook/useAuth";
+import toast from "react-hot-toast";
 
 const ReviewCard = ({ review, onDelete }) => {
+  const [data, setData] = useState(null);
+  const [ratings, setRatings] = useState(0);
+  const [open, setOpen] = useState(false);
+  const { user } = useAuth();
+  const handleOpen = () => setOpen(!open);
+  const navigate = useNavigate();
   const {
     _id,
     postId,
@@ -27,7 +46,6 @@ const ReviewCard = ({ review, onDelete }) => {
     currentUserName,
     currentUserProfileImage,
   } = review;
-
 
   const handelDelete = (id) => {
     Swal.fire({
@@ -55,57 +73,173 @@ const ReviewCard = ({ review, onDelete }) => {
     });
   };
 
+  const handelModal = (id) => {
+    axios.get(`http://localhost:5000/review/${id}`).then((res) => {
+      setData(res.data);
+      handleOpen();
+    });
+  };
+
+  const handleReview = (e) => {
+    e.preventDefault();
+    const comment = e.target.comment.value;
+    const currentUserName = user?.displayName;
+    const currentUserProfileImage = user?.photoURL;
+    const postId = _id;
+    const updatedData = {
+      postId,
+      image,
+      title,
+      website,
+      comment,
+      rating: ratings,
+      postedDate,
+      userEmail,
+      currentUserName,
+      currentUserProfileImage,
+    };
+
+    if (ratings === 0) {
+      return toast.error("Add Rating Star!");
+    }
+
+    // send data mongodb
+    try {
+      axios.put(`http://localhost:5000/review/${_id}`, updatedData);
+      Swal.fire({
+        position: "top-center",
+        icon: "success",
+        title: "Your review updated successfully",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      setOpen(!open);
+      navigate("/")
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <Card
-      color="transparent"
-      shadow={false}
-      className="w-full border p-5 pb-0 shadow-xl shadow-green-50"
-    >
-      <CardHeader
+    <>
+      <Card
         color="transparent"
-        floated={false}
         shadow={false}
-        className="mx-0 flex items-center gap-4 pt-0 pb-8"
+        className="w-full border p-5 pb-0 shadow-xl shadow-green-50"
       >
-        <Avatar size="lg" variant="circular" src={currentUserProfileImage} />
-        <div className="flex w-full flex-col gap-0.5">
-          <div className="flex items-center justify-between">
-            <Typography variant="h5" color="blue-gray">
-              {currentUserName}
+        <CardHeader
+          color="transparent"
+          floated={false}
+          shadow={false}
+          className="mx-0 flex items-center gap-4 pt-0 pb-8"
+        >
+          <Avatar size="lg" variant="circular" src={currentUserProfileImage} />
+          <div className="flex w-full flex-col gap-0.5">
+            <div className="flex items-center justify-between">
+              <Typography variant="h5" color="blue-gray">
+                {currentUserName}
+              </Typography>
+              <div className="5 flex items-center gap-0">
+                <Rating
+                  style={{ maxWidth: 250 }}
+                  value={rating}
+                  readonly
+                  unratedColor="green"
+                  ratedColor="green"
+                />
+              </div>
+            </div>
+            <Typography color="blue-gray">
+              {format(new Date(postedDate), "Pp")}
             </Typography>
-            <div className="5 flex items-center gap-0">
+          </div>
+        </CardHeader>
+        <CardBody className="mb-6 p-0">
+          <Typography>{comment.slice(0, 200)}...</Typography>
+
+          <div className="divider"></div>
+          <div className="flex items-center justify-between gap-4">
+            <Avatar size="lg" variant="rounded" src={image} />
+            <Link to={`/service/${postId}`} className="flex-1">
+              <h1 className="text-black font-medium">{title}</h1>
+              <p>{website}</p>
+            </Link>
+          </div>
+          <div className="flex items-center justify-end gap-5">
+            <FiEdit3
+              onClick={() => handelModal(_id)}
+              className="text-2xl text-green-500 cursor-pointer"
+            />
+
+            <HiOutlineTrash
+              onClick={() => handelDelete(_id)}
+              className="text-2xl text-red-500 cursor-pointer"
+            />
+          </div>
+        </CardBody>
+      </Card>
+
+      <Dialog size="lg" open={open} handler={handleOpen}>
+        <DialogHeader className="justify-end pt-3">
+          <IconButton
+            color="blue-gray"
+            size="sm"
+            variant="text"
+            onClick={handleOpen}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+              className="h-5 w-5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </IconButton>
+        </DialogHeader>
+        <div className="md:px-10 p-5">
+          <form onSubmit={handleReview} className="flex flex-col gap-4 ">
+            <div>
+              <h1 className="font-bold text-2xl md:text-3xl mb-2">
+                Update Review
+              </h1>
+            </div>
+            <div>
+              <Textarea
+                defaultValue={data?.comment}
+                name="comment"
+                label="Comment"
+                style={{ height: 200 }}
+                success
+                required
+              />
+            </div>
+            <div className="flex items-center gap-2">
+              <h5 className="font-semibold">Give Rating : </h5>
               <Rating
-                style={{ maxWidth: 250 }}
+                defaultValue={data?.rating}
+                style={{ maxWidth: 150 }}
                 value={rating}
-                readonly
+                onChange={setRatings}
                 unratedColor="green"
                 ratedColor="green"
               />
             </div>
-          </div>
-          <Typography color="blue-gray">
-            {format(new Date(postedDate), "Pp")}
-          </Typography>
+            <div className="text-end">
+              <Button type="submit" className="bg-green-500">
+                Update Review
+              </Button>
+            </div>
+          </form>
         </div>
-      </CardHeader>
-      <CardBody className="mb-6 p-0">
-        <Typography>{comment.slice(0, 200)}...</Typography>
-
-        <div className="divider"></div>
-        <div className="flex items-center justify-between gap-4">
-          <Avatar size="lg" variant="rounded" src={image} />
-          <Link to={`/service/${postId}`} className="flex-1">
-            <h1 className="text-black font-medium">{title}</h1>
-            <p>{website}</p>
-          </Link>
-        </div>
-        <div className="flex items-center justify-end gap-5">
-          <FiEdit3 className="text-2xl text-green-500 cursor-pointer" />
-
-          <HiOutlineTrash  onClick={() => handelDelete(_id)} className="text-2xl text-red-500 cursor-pointer" />
-        </div>
-      </CardBody>
-    </Card>
+      </Dialog>
+    </>
   );
 };
 
